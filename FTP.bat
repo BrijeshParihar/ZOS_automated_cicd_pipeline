45b6d35fde90b016a0e0c90feb38703946e2420c
bparihar <brijesh.parihar@soprabanking.com>
2022-12-04 23:03:08 +0530

    Required files updated.

--- FTP.bat
+++ FTP.bat
@@ -1,5 +1,8 @@
-cd C:\Users\user1\Desktop
-ftp -s:FTP.txt
+cd C:\Brijesh\ZOS_automated_cicd_pipeline
+echo done..................
+sftp S152945@zptxevolan01.ptx.fr.sopra
+sep@sep
+echo done1..........
 xcopy /y REXX001.txt "C:\Brijesh\ZOS_automated_cicd_pipeline"
 del REXX001.txt
 
---
+++ README.md
@@ -0,0 +1,1 @@
+((((( FILE Moved in local machine ))))) Sun 12/04/2022 23:03:08.04
